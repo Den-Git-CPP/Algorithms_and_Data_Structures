7e8b254334b9b3560be1ae4aa2f9ee108f8b6e1a
Den-Git-CPP <madim-neodart@rambler.ru>
2023-04-11 22:49:31 +0300

    cout Stack 743

--- 2.Data Structures/stack.cpp
+++ 2.Data Structures/stack.cpp
@@ -2,9 +2,24 @@
 #include<stack>
 #include<string>
 
-using namespace std;
+
 
 int main()
 {
+stack<int> st;
+st.push(2);
+st.push(7);
+std::cout<<st.top(); //7
+st.top();
+st.pop();
+st.push(3);
+st.push(4);
 
+std::cout<<st.top();//4
+st.pop();
+
+std::cout<<st.top();//3
+st.pop();
+
+return 0;
 }

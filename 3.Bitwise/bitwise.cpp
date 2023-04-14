10e5428a135474ccb542e6c5b8e37b6756e5066a
Den-Git-CPP <madim-neodart@rambler.ru>
2023-04-14 18:51:46 +0300

    First commit bitwise

---
+++ 3.Bitwise/bitwise.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+    cout << (51 & 25) << endl; // 17
+    cout << (51 | 25) << endl; // 59
+    cout << (51 ^ 25) << endl; // 42
+    cout << (~23u) << endl;
+    cout << endl;
+    cout << 12 << 2 << endl;
+    cout << (12 << 2) << endl;
+    cout << (12 >> 2) << endl;
+    cout << (1 << 4) << endl;
+    cout << (1 << 5) << endl;
+    cout << (1 << 6) << endl;
+    cout << (1 << 7) << endl;
+
+    
+}
